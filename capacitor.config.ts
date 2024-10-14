30bd0fb84d833875a90618c4a61f0b360a5731d4
jgustavobarbosa <149620117+jgustavobarbosa@users.noreply.github.com>
2024-10-13 22:28:26 -0300

    commit geral

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'br.com.judje',
+  appName: 'JUDJe',
+  webDir: 'www'
+};
+
+export default config;
